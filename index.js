17ee6d047956646c3404cb5981614daed2a041a4
Rafafrias2012 <155685529+Rafafrias2012@users.noreply.github.com>
2024-06-12 11:27:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const sanitizeHtml = require('sanitize-html');
 
 let users = {};
 let rooms = {};
